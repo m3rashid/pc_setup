97b78011ad5c8648f3ac615cfc99ca9c8255c2e2
MD Rashid Hussain <64685317+m3rashid@users.noreply.github.com>
2023-03-01 08:48:35 +0530

    Update setup_dbs.sh

--- scripts/setup_dbs.sh
+++ scripts/setup_dbs.sh
@@ -57,3 +57,13 @@ supervised systemd # probabbly on line 147 (earlier it would be supervised no, c
 sudo systemctl restart redis.service
 sudo systemctl status redis
 redis-cli ping # to test the installation
+
+#######################
+### Set up InfluxDb ###
+#######################
+wget -q https://repos.influxdata.com/influxdata-archive_compat.key
+echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
+
+sudo apt-get update && sudo apt-get install influxdb2
+
