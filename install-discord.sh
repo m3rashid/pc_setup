171c72574af7368c5e15a0e917fe51a0e9e74890
MD Rashid Hussain <64685317+m3rashid@users.noreply.github.com>
2023-05-29 08:11:18 +0530

    Create install-discord.sh

---
+++ install-discord.sh
@@ -0,0 +1,10 @@
+# Download the discord debian package using the following command.
+ -wget "https://discord.com/api/download?platform=linux&format=deb" -O discord.deb
+
+    ## If wget command is not found then proceed after executing the following command
+      -sudo apt update
+      -sudo apt install wget
+
+# Now execute it using the following command and you are done.
+ -sudo apt install ./discord.deb -y
+ 
