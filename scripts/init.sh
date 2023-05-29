d34e47a3b5ae82f1981002279e85a515e0393602
MD Rashid Hussain <64685317+m3rashid@users.noreply.github.com>
2023-05-29 08:04:32 +0530

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -12,7 +12,13 @@ echo "Seting up TMUX config";
 curl -i -X GET https://raw.githubusercontent.com/m3rashid/pc_setup/main/dotfiles/.tmux.conf | cat > ~/.tmux.conf
 echo "Done";
 
+echo "Setting up ZSH Config";
+# TODO
+echo "Done";
+
 echo "Seting up Github Cli Login";
 mkdir -p ~/code/configs/.gitLogin
 curl -X GET https://raw.githubusercontent.com/m3rashid/pc_setup/main/scripts/gh.sh | cat > ~/code/configs/.gitLogin/setup.sh
+echo "source ~/code/myStuff/.gitLogin/github_login.sh" >> ~/.bashrc
+echo "source ~/code/myStuff/.gitLogin/github_login.sh" >> ~/.zshrc
 echo "Done";
