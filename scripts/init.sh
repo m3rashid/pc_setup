4708db5d8403083a0a062063ee6621f5b0b59fb0
MD Rashid Hussain <64685317+m3rashid@users.noreply.github.com>
2023-05-29 08:02:40 +0530

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,12 +2,17 @@ echo "MD Rashid Hussain";
 
 echo "Cleanint up tasks";
 curl -X GET https://raw.githubusercontent.com/m3rashid/pc_setup/main/scripts/cleanups.sh
-echo "Cleanup Done";
+echo "Done";
 
 echo "Installing prefernces";
 curl -X GET https://raw.githubusercontent.com/m3rashid/pc_setup/main/scripts/install-my-preferences.sh
-echo "Prefernces installed";
+echo "Done";
 
 echo "Seting up TMUX config";
 curl -i -X GET https://raw.githubusercontent.com/m3rashid/pc_setup/main/dotfiles/.tmux.conf | cat > ~/.tmux.conf
-echo "Tmux Config setup Done";
+echo "Done";
+
+echo "Seting up Github Cli Login";
+mkdir -p ~/code/configs/.gitLogin
+curl -X GET https://raw.githubusercontent.com/m3rashid/pc_setup/main/scripts/gh.sh | cat > ~/code/configs/.gitLogin/setup.sh
+echo "Done";
