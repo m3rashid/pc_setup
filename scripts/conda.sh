b1e4f5aa41343f79dfdd5f6bdc6ff111de880dba
MD Rashid Hussain <64685317+m3rashid@users.noreply.github.com>
2023-08-14 12:28:05 +0530

    Create conda.sh

---
+++ scripts/conda.sh
@@ -0,0 +1,12 @@
+cd Downloads
+wget https://repo.anaconda.com/archive/Anaconda3-2023.07-2-Linux-x86_64.sh # or download the latest release from the anaconda download page
+chmod +x ./Anaconda3-2023.07-2-Linux-x86_64.sh
+./Anaconda3-2023.07-2-Linux-x86_64.sh
+
+# Use the defaults untill this section arrives
+# If you'd prefer that conda's base environment not be activated on startup, 
+#   set the auto_activate_base parameter to false: 
+# conda config --set auto_activate_base false
+# Thank you for installing Anaconda3!
+
+
