1a472814fcd84e82eda53a02a3d210c10f1269c4
MD Rashid Hussain <64685317+m3rashid@users.noreply.github.com>
2023-10-16 13:28:02 +0530

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -19,6 +19,6 @@ echo "Done";
 echo "Seting up Github Cli Login";
 mkdir -p ~/code/configs/.gitLogin
 curl -X GET https://raw.githubusercontent.com/m3rashid/pc_setup/main/scripts/gh.sh | cat > ~/code/configs/.gitLogin/setup.sh
-echo "source ~/code/myStuff/.gitLogin/github_login.sh" >> ~/.bashrc
-echo "source ~/code/myStuff/.gitLogin/github_login.sh" >> ~/.zshrc
+echo "source ~/code/configs/.gitLogin/github_login.sh" >> ~/.bashrc
+echo "source ~/code/configs/.gitLogin/github_login.sh" >> ~/.zshrc
 echo "Done";
